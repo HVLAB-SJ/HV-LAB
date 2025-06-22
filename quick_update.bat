8340606c5f6969157267c5d66a61e784355ba2a3
HVLAB-SJ <HVLAB-SJ@users.noreply.github.com>
2025-06-22 14:29:26 +0900

    v1.1

--- quick_update.bat
+++ quick_update.bat
@@ -1,9 +1,12 @@
 @echo off
-set /p V="버전: "
+chcp 65001 > nul
+echo.
+set /p V="Version: "
 git add .
 git commit -m "v%V%"
 git push
 git tag v%V%
 git push origin v%V%
+echo.
 echo Done! Check https://github.com/HVLAB-SJ/HV-LAB/actions
 pause 
